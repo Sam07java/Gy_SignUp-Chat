dfc01df6a870522e267cc7879e06471650655121
Sam07java <sameersponnad@gmail.com>
2024-12-08 19:43:19 +0530

    Second commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -11,22 +11,6 @@ import org.junit.runner.RunWith;
 )
 public class TestRunner {
     ///login_validation_001.feature
-//Round --1
-    //  1-->> git init -->>create a new local repository<<--
-
-    //  2-->> git config --global user.name "your name"
-    //        git config --global user.email "your email"
-
-    //  3-->> git status -->>shows untracked file, red colur fond shown code is in our local system<<--
-
-    //  {4-->> git add -A -->>add all  the files to staging<<--
-    //         git add filename
-    //         git add *.java
-    //         git add filename<<--}
-
-    //  5-->> git commit -m "First commit"  -->>commit the code into Local(git) repository<<--
-    //  git status
-    //
 
 
 
